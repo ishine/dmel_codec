29a8ec145d677fc95b90a6c9c5a1bd2b60aa5a28
wuzhiyue111 <347100518@qq.com>
2024-12-14 10:59:37 +0000

    [dmel_codec env build] fix related import and add readme

--- dmel_codec/models/modules/bigvgan/alias_free_activation/cuda/activation1d.py
+++ dmel_codec/models/modules/bigvgan/alias_free_activation/cuda/activation1d.py
@@ -3,10 +3,10 @@
 
 import torch
 import torch.nn as nn
-from ..torch.resample import UpSample1d, DownSample1d
+from dmel_codec.models.modules.bigvgan.alias_free_activation.torch.resample import UpSample1d, DownSample1d
 
 # load fused CUDA kernel: this enables importing anti_alias_activation_cuda
-from . import load
+from dmel_codec.models.modules.bigvgan.alias_free_activation.cuda import load
 
 anti_alias_activation_cuda = load.load()
 
