8b51c007a8df58e39831b96ab1ad3380b299c03e
wuzhiyue111 <347100518@qq.com>
2024-12-23 10:08:13 +0000

    fix config yaml 多级调用

--- dmel_codec/train.py
+++ dmel_codec/train.py
@@ -3,24 +3,32 @@ import multiprocessing
 from typing import List
 
 import lightning.pytorch as pl
-import rootutils
-from lightning.pytorch.loggers import Logger
-from omegaconf import DictConfig
 
+from lightning.pytorch.loggers import Logger
+from omegaconf import DictConfig, OmegaConf
+import dmel_codec
 from dmel_codec.utils.logger import RankedLogger
 from dmel_codec.utils.print_config import print_config_tree
 from dmel_codec.utils.utils import find_lastest_ckpt
 from dmel_codec.utils.instantiators import instantiate_loggers
 
-
-rootutils.setup_root(__file__, indicator=".dMel_chatMusic_root", pythonpath=True)
-
 logger = RankedLogger(__name__, rank_zero_only=True)
 
+def get_config():
+    try:
+        base_path = dmel_codec.__path__[0]
+        base_config = OmegaConf.load(f"{base_path}/config/dMel_used.yaml")
+        for special_config_path in base_config.defaults[1:]:
+            special_config = OmegaConf.load(f"{base_path}/config/{special_config_path}")
+            base_config = OmegaConf.merge(base_config, special_config)
+        return base_config
 
-# TODO wzy_config/zh_config/lc_config 文件夹的路径
-@hydra.main(config_path="config", config_name="dMel_example", version_base=None)
+    except Exception as e:
+        logger.error(f"Error loading config: {base_config.defaults[1:]=}, {e=}")
+        raise e
+
 def main(config: DictConfig) -> None:
+    
     print_config_tree(config)
 
     pl.seed_everything(config.seed)
@@ -54,4 +62,5 @@ def main(config: DictConfig) -> None:
 
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn")
-    main()
+    cfg = get_config()
+    main(cfg)
